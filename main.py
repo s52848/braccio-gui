de54d43be67335e2f20810062a67114754b208bc
s52848 <52848@student.umg.edu.pl>
2026-03-03 17:58:48 +0100

    feat: add conceptual app layout

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 from kivy.app import App
-from kivy.properties import StringProperty
 from kivy.uix.boxlayout import BoxLayout
 
 
-class MainLayout(BoxLayout):
-    label_text = StringProperty("Witaj w aplikacji!")
-
-    def on_button_click(self, number):
-        self.label_text = f"Naciśnięto przycisk {number}"
+class BraccioRoot(BoxLayout):
+    pass
 
 
-class MyApp(App):
+class BraccioApp(App):
     def build(self):
-        return MainLayout()
+        return BraccioRoot()
 
 
 if __name__ == "__main__":
-    MyApp().run()
+    BraccioApp().run()
